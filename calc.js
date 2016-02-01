568b3108eef65b6c7732a33c78b582d70c35915a
Merit Paist <merit26@users.noreply.github.com>
2016-02-01 12:16:57 +0200

    working calculator

---
+++ calc.js
@@ -0,0 +1,16 @@
+
+window.onload = function () {
+//saan kätte input väljad
+var cm = document.getElementById('cm');
+var inch = document.getElementById('inch');
+
+// lisan nuppudele kuularid
+  document.getElementById('to_inch').addEventListener('click', convertToInch);
+};
+
+function convertToInch(){
+  //arvutan ümber tollideks ja lisan väärtuse tollid lahtrisse
+  inch.value = cm.value/2.54;
+
+
+}
